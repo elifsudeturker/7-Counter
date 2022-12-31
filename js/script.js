3ec1ebf4ea6ebdcf60d2c86d34ae11eaa849fbce
elifsude <elifsudeturker@hotmail.com>
2022-12-31 22:04:41 +0300

    0-10 arası sayma ve mobil uyumluluk eklendi.

--- js/script.js
+++ js/script.js
@@ -1,13 +1,20 @@
 function increase(){
     var number;
     number = document.getElementById('count').value
+    if(number<11)
     number= parseInt(number) + 1
+    if(number==11)
+    number= 0
     document.getElementById('count').value = number
 }
 
 function decrease(){
     var number;
     number = document.getElementById('count').value
+    if(number>-1)
     number= parseInt(number) - 1
+    if(number==-1)
+    number=10
     document.getElementById('count').value = number
 }
+
